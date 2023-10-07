b30eaabd993022c1d7df143ca546a24a7e7a7f61
Marcos Rosa <marcos.rosa@mindera.com>
2023-10-07 23:52:55 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
         'Topic :: Software Development :: Build Tools',
 
         # Pick your license as you wish
-        'License :: GNU General Public Licience',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
